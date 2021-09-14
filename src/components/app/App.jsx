d4cf57fb5a771c3331c47bdb1d257f225077e4d5
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 10:38:38 -0700

    built out app router component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+// eslint-disable-next-line max-len
+import { RickAndMortyDetailContainer, RickAndMortyContainer } from '../../containers/RickAndMortyContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={RickAndMortyContainer} />
+        <Route exact path="/:id" component={RickAndMortyDetailContainer} />
+      </Switch>
+    </Router>
+  );
 }
