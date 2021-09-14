a76d2c0e21e814f351feb2f458e44ad5d979a26d
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 10:59:28 -0700

    built out fetchbyid container

--- src/containers/RickAndMortyDetailContainer.jsx
+++ src/containers/RickAndMortyDetailContainer.jsx
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 import { fetchCharactersById } from '../services/rickAndMortyApi';
 
 export default class RickAndMortyDetailContainer extends Component {
+  state = {
+    loading: true,
+    character: {},
+  }
+
+  componentDidMount() {
+    fetchCharactersById(this.state.match.props.id)
+      .then((character) =>
+        this.setState({ character, loading: false })
+      );
+  }
+
   render() {
-    return <CharacterDetail />;
+
+    const { loading, character } = this.state;
+
+    if(loading) {
+      return (
+        <img
+          src="https://loading.io/icon/pd8fa5"
+          alt="loading spinner"
+        />
+      );
+    }
+
+    return <CharacterDetail character={character} />;
   }
 }
