40b5a7b7875c02f719bbaf3d232c94617e1ad10f
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-14 10:52:10 -0700

    built out character component

--- src/containers/RickAndMortyContainer.jsx
+++ src/containers/RickAndMortyContainer.jsx
@@ -3,7 +3,29 @@ import React, { Component } from 'react';
 import { fetchCharacters } from '../services/rickAndMortyApi';
 
 export default class RickAndMortyContainer extends Component {
+  state = {
+    loading: true,
+    characters: [],
+  }
+
+  componentDidMount() {
+    fetchCharacters()
+      .then((characters) =>
+        this.setState({ characters, loading: false })
+      );
+  }
+
   render() {
-    return <CharacterList />;
+    const { loading, characters } = this.state;
+
+    if(loading) {
+      return (
+        <img
+          src="https://loading.io/icon/pd8fa5"
+          alt="loading spinner" />
+      );
+    }
+    
+    return <CharacterList characters={characters} />;
   }
 }
